1f5ad3f48e99a29a6d82ca121c289c648f8d0887
Lingxiang LinG Wang <lingxiang.wang@bytedance.com>
2020-09-23 13:11:49 -0400

    wrong proj1
    Change-Id: Ie885efcb0a73bd3a1c4b31f1ce8027e84271f5e9

--- pingcap-rust-project-1/src/bin/kvs.rs
+++ pingcap-rust-project-1/src/bin/kvs.rs
@@ -1,51 +1,47 @@
-use clap::{App, Arg, SubCommand, AppSettings};
+use clap::{App, Arg, SubCommand};
 use kvs::KvStore;
 use std::process;
-use std::process::exit;
+use std::intrinsics::unreachable;
 
 fn main() {
     let matches = App::new("kvs")
-        .version(env!("CARGO_PKG_VERSION"))
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .about(env!("CARGO_PKG_DESCRIPTION"))
-        .setting(AppSettings::DisableHelpSubcommand)
-        .setting(AppSettings::SubcommandRequiredElseHelp)
-        .setting(AppSettings::VersionlessSubcommands)
+        .version("0.1.0")
         .subcommand(
             SubCommand::with_name("get").
                 arg(
-                    Arg::with_name("KEY").help("").required(true)
+                    Arg::with_name("KEY").required(true)
                 )
         )
         .subcommand(
             SubCommand::with_name("set").
                 arg(
-                Arg::with_name("KEY").help("").required(true)
+                Arg::with_name("KEY").required(true)
                 )
                 .arg(
-                    Arg::with_name("VALUE").help("").required(true)
+                    Arg::with_name("VALUE").required(true)
                 )
         )
         .subcommand(
             SubCommand::with_name("rm")
                 .arg(
-                Arg::with_name("KEY").value_name("rm").help("").required(true)
+                Arg::with_name("KEY").value_name("rm").required(true)
                 )
         )
         .get_matches();
 
     match matches.subcommand() {
         ("get", Some(_matches)) => {
-            eprintln!("unimplemented");
-            exit(1);
+            // unimplemented!()
+            eprintln!("unimlpemented");
+            process::exit(1);
         },
         ("set", Some(_matches)) => {
-            eprintln!("unimplemented");
-            exit(1);
+            eprintln!("unimlpemented");
+            process::exit(1);
         },
         ("rm", Some(_matches)) => {
-            eprintln!("unimplemented");
-            exit(1);
+            eprintln!("unimlpemented");
+            process::exit(1);
         },
         _ => {
             unreachable!()
@@ -53,4 +49,3 @@ fn main() {
     }
 
 }
-
